b2c61629a36f7f53ebd28588ac1df34466ba3f7c
alkaff166 <150617892+alkaff166@users.noreply.github.com>
2024-01-06 12:58:14 -0500

    Update Test.java

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	@org.testng.annotations.Test
 	public void loginTest() {
-		
+		// Adding a new comment 1
 		System.out.println("wr are learning Git and GitHub through Eclipse.");
 		System.out.println("we had our initial commit.");
 		
