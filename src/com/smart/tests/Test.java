fd547de0c9852eb78136ef1ea08433373691b053
alkaff166 <alkaff166@gmail.com>
2024-01-06 12:24:27 -0500

    Added a new print statement

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -2,4 +2,10 @@ package com.smart.tests;
 
 public class Test {
 
+	@org.testng.annotations.Test
+	public void loginTest() {
+		
+		System.out.println("wr are learning Git and GitHub through Eclipse.");
+	}
+	
 }
